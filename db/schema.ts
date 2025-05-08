a6d11713e4455edd6bbf0219fcb13cf5e77ccac0
leehyungwon <hueleev@dkitec.com>
2025-05-08 17:35:21 +0900

    drizzle-orm & neon

--- db/schema.ts
+++ db/schema.ts
@@ -18,11 +18,36 @@ export const userRelations = relations(user, ({ many }) => ({
 export const conversation = pgTable("conversation", {
   id: uuid("id").defaultRandom().notNull().primaryKey(),
   name: text("name"),
-  userId: uuid("userId"),
+  userId: uuid("userId")
+    .references(() => user.id, { onDelete: "cascade" })
+    .notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
-export const conversationsRelations = relations(conversation, ({ one }) => ({
-  user: one(user, { fields: [conversation.userId], references: [user.id] }),
+export const conversationRelations = relations(
+  conversation,
+  ({ one, many }) => ({
+    user: one(user, { fields: [conversation.userId], references: [user.id] }),
+    messages: many(message),
+  })
+);
+
+export const message = pgTable("message", {
+  id: uuid("id").defaultRandom().notNull().primaryKey(),
+  content: text("content"),
+  role: text("role").$type<"user" | "assistant">(),
+  conversationId: uuid("conversationId")
+    .references(() => conversation.id, { onDelete: "cascade" })
+    .notNull(),
+
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const messageRelations = relations(message, ({ one }) => ({
+  conversation: one(conversation, {
+    fields: [message.conversationId],
+    references: [conversation.id],
+  }),
 }));
